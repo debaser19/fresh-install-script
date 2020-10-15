cac57b5de716588441e203b09360133cdcd28251
debaser19 <steve@stoveld.xyz>
2020-10-14 20:15:33 -0700

    windows script
    installs chocolatey and packages

---
+++ install.ps1
@@ -0,0 +1,12 @@
+﻿# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+ if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+  Exit
+ }
+}
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install -y brave spotify vlc discord telegram vscode pycharm-community python nextcloud-client github-desktop sourcetree irfanview sharex teracopy revo-uninstaller launchy windirstat foxitreader notepadplusplus sublimetext3 winscp audacity 7zip
